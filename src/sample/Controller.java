5fa39931f0f1f4fd9c685728838894825b4d8c9b
msosic97 <msosic97@gmail.com>
2020-03-11 12:35:25 +0100

    Zbog vjezbe dodao sam neke actions za Button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField nameField;
+    public TextField emailField;
+    public PasswordField passField;
+
+    public void Button(ActionEvent actionEvent) {
+        nameField.setText("Muhamed");
+        emailField.setText("msosic1@etf.unsa.ba");
+        passField.setText("0000");
+    }
 }
+//Zbog vjezbe dodao sam neke actions za Button
